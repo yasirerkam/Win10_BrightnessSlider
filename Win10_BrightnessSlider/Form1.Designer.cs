51f6dcb2ec4b40aeb0112bbf2f5ff570a6d15e37
LIVE2RISE <LIVE2RISE@LIVE2RISE>
2017-09-28 22:11:41 +0300

    Add project files.

---
+++ Win10_BrightnessSlider/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Win10_BrightnessSlider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(59, 27);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(246, 23);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(311, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 66);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Win10_BrightnessSlider/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Management; //add dll to reference
+using Microsoft.Win32;
+
+namespace Win10_BrightnessSlider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //override form as toolwindow
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x80;
+                return cp;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = false;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width +22 ,
+                Screen.PrimaryScreen.WorkingArea.Height +22 );
+
+
+            //colors
+            BackColor = Color.FromArgb(31, 31, 31);
+            label1.ForeColor = Color.White;
+
+            //form show hide event
+            notifyIcon1.MouseClick += NotifyIcon1_MouseClick;
+            //clicked outside of form
+            Deactivate += Form1_Deactivate;
+
+
+            CreateNotifyIConContexMenu();
+            UpdateStatesOnGuiControls();
+        }
+
+        private void CreateNotifyIConContexMenu()
+        {
+
+            var cm = new ContextMenu();
+            var mi1 = new MenuItem("Exit", (snd, ev) => {
+                Application.Exit();
+            });
+
+            var mi2 = new MenuItem("State Of Window", (snd, ev) => {
+                var msg =
+                "visible:" + this.Visible + "\r\n" +
+                "Focused:" + this.Focused + "\r\n" +
+                "canFocus:" + this.CanFocus + "\r\n";
+                MessageBox.Show(msg);
+            });
+
+            var mi3 = new MenuItem("Run At Startup", (snd, ev) => {
+                var _mi3 = snd as MenuItem;
+
+                _mi3.Checked = !_mi3.Checked; // toggle
+
+                SetStartup(_mi3.Checked);
+            });
+
+            cm.MenuItems.Add(mi1);
+            cm.MenuItems.Add(mi3);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                cm.MenuItems.Add(mi2);
+            }
+
+            notifyIcon1.ContextMenu = cm;
+        }
+        private void UpdateStatesOnGuiControls()
+        {
+            //get current states
+            var isRunSttup = isRunAtStartup();
+            notifyIcon1.ContextMenu.MenuItems
+                .Cast<MenuItem>().Where( x=> x.Text == "Run At Startup").FirstOrDefault()
+                .Checked = isRunSttup;
+
+            var initBrig = GetBrightness();
+            label1.Text = initBrig + "";
+            trackBar1.Value = initBrig;
+        }
+
+
+        bool vis = false;
+        public void eSetVis(bool visible)
+        {
+            Console.WriteLine("eSetVis - vis:" + vis);
+            this.WindowState = FormWindowState.Normal;
+            this.StartPosition = FormStartPosition.Manual;
+
+            var scrWA = Screen.PrimaryScreen.WorkingArea;
+            var p = new Point(scrWA.Width , scrWA.Height );
+
+            if (visible)
+            {
+                p.Offset(-this.Width, -this.Height);
+                this.Location = p;
+
+                this.Activate();
+                this.Show();
+                this.BringToFront();
+
+                vis = true;
+            }
+            else
+            {
+                p.Offset(this.Width, this.Height);
+                this.Location = p;
+
+                vis = false;
+
+            }
+
+
+
+        }
+       
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            eSetVis(false);
+        }
+        
+        private void NotifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.MouseClick -= NotifyIcon1_MouseClick;
+            Deactivate -= Form1_Deactivate;
+
+            if ( e.Button == MouseButtons.Left)
+            {
+                Console.WriteLine("Notify Cliiked - vis:" + vis);
+
+                eSetVis(!vis);
+            }
+           
+
+            notifyIcon1.MouseClick += NotifyIcon1_MouseClick;
+            Deactivate += Form1_Deactivate;
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            byte g = 0;
+            if (byte.TryParse(trackBar1.Value + "", out g))
+            {
+                SetBrightness(g);
+                label1.Text = g + "";
+            }
+
+
+        }
+
+        static void SetBrightness(byte targetBrightness)
+        {
+            ManagementScope scope = new ManagementScope("root\\WMI");
+            SelectQuery query = new SelectQuery("WmiMonitorBrightnessMethods");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+            {
+                using (ManagementObjectCollection objectCollection = searcher.Get())
+                {
+                    foreach (ManagementObject mObj in objectCollection)
+                    {
+                        mObj.InvokeMethod("WmiSetBrightness",
+                            new Object[] { UInt32.MaxValue, targetBrightness });
+                        break;
+                    }
+                }
+            }
+        }
+        static int GetBrightness()
+        {
+            ManagementScope scope = new ManagementScope("root\\WMI");
+            SelectQuery query = new SelectQuery("WmiMonitorBrightness");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+            {
+                using (ManagementObjectCollection objectCollection = searcher.Get())
+                {
+                    foreach (ManagementObject mObj in objectCollection)
+                    {
+                        var br_obj = mObj.Properties["CurrentBrightness"].Value;
+
+                        int br = 0;
+                        int.TryParse(br_obj+"", out br);
+                        return br;
+                        break;
+                    }
+                }
+            }
+            return 0;
+
+        }
+
+        private void SetStartup( bool RunAtStartup )
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (RunAtStartup)
+                rk.SetValue(  Application.ProductName, Application.ExecutablePath );
+            else
+                rk.DeleteValue(Application.ProductName , false);
+
+        }
+        private bool isRunAtStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            var  val  = rk.GetValue(Application.ProductName );
+
+            if (val+"" == Application.ExecutablePath  )
+                return true;
+            else
+                return false;
+
+        }
+
+
+
+    }
+
+
+    
+}
+
+
+
+
+
+ 
